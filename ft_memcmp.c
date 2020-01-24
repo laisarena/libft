89a9a954681c0b6f220112c9f3a40c14cbdb445f
Lais Frasson arena <lfrasson@c1r4p1.42sp.org.br>
2020-01-24 15:59:44 -0300

    ft_memcmp

---
+++ ft_memcmp.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lfrasson <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/01/24 15:06:38 by lfrasson          #+#    #+#             */
+/*   Updated: 2020/01/24 15:59:06 by lfrasson         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+	const unsigned char	*ptrs1;
+	const unsigned char	*ptrs2;
+
+	ptrs1 = (const unsigned char *)s1;
+	ptrs2 = (const unsigned char *)s2;
+	while (n--)
+	{
+		if (*ptrs1 != *ptrs2)
+			return (*ptrs1 - *ptrs2);
+		ptrs1++;
+		ptrs2++;
+	}
+	return (0);
+}
