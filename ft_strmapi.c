71d57f21b8f26e09aedf8a7fc24a3ebb53136999
Lais Frasson arena <lfrasson@c1r6p4.42sp.org.br>
2020-02-18 14:28:20 -0300

    strmapi -> retorna NULL caso a string de entrada seja NULL

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -6,7 +6,7 @@
 /*   By: lfrasson <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/23 11:02:24 by lfrasson          #+#    #+#             */
-/*   Updated: 2020/02/18 13:02:35 by lfrasson         ###   ########.fr       */
+/*   Updated: 2020/02/18 14:23:15 by lfrasson         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,13 @@ char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 	char			*str;
 	unsigned int	i;
 
-	if (!(str = malloc(sizeof(char) * ft_strlen(s))))
+	if (!s)
+		return (NULL);
+	if (!(str = malloc(sizeof(char) * (ft_strlen(s) + 1))))
 		return (0);
 	i = 0;
-	while (s[i])
-	{
-		str[i] = f(i, s[i]);
-		i++;
-	}
-	return (str);
+	while (*s)
+		*str++ = f(i++, *s++);
+	*str = '\0';
+	return (str - i);
 }
