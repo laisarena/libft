21bfd74b652ec3346c9a27d01f139d77ffc3c97c
Lais Frasson arena <lfrasson@c1r6p3.42sp.org.br>
2020-03-03 17:31:40 -0300

    Correção de Abort da substr

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: lfrasson <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/22 11:11:42 by lfrasson          #+#    #+#             */
-/*   Updated: 2020/02/18 13:51:10 by lfrasson         ###   ########.fr       */
+/*   Updated: 2020/03/03 17:31:03 by lfrasson         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,17 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char	*str;
 	char	*ptr;
+	size_t	slen;
 
 	if (!s)
 		return (NULL);
 	if (!(str = malloc(sizeof(char) * (len + 1))))
 		return (NULL);
-	s = s + start;
+	slen = ft_strlen(s);
+	if (start >= slen)
+		s = s + slen;
+	else
+		s = s + start;
 	ptr = str;
 	while (*s && len--)
 		*str++ = *s++;
